830b7da436ae690cc0f80fc7a336fafd01cdfe40
karavainastya <134556049+karavainastya@users.noreply.github.com>
2023-06-13 22:17:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 import sklearn
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, precision_score, recall_score, roc_auc_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
+
+#from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+
 import streamlit as st
 import pickle
-
+import numpy as np
 
 import base64
 @st.cache(allow_output_mutation=True)
