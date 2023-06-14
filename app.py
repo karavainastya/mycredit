1cc5b9103b2b87a1b5c7d4afea870766f016bec8
karavainastya <134556049+karavainastya@users.noreply.github.com>
2023-06-15 01:51:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def main():
     NAME_INCOME_TYPE_Maternity_leave= st.selectbox('Тип дохода: В декретном отпуске',['0', '1']) 
     
     if int(NAME_INCOME_TYPE_Working)==1: 
-        int(NAME_INCOME_TYPE_State_servant)& int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Pensioner)& int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Businessman)&
+        int(NAME_INCOME_TYPE_State_servant) & int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Pensioner) & int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Businessman) &
     int(NAME_INCOME_TYPE_Businessman)!=1
         
     REGION_RATING_CLIENT= st.selectbox('Рейтинг региона проживания клиента: Примечание: 1 - Минск, 2 - областные центры, 3 - остальные населенные пункты.', ['1', '2', '3'])            
