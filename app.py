bfd94e9f0e93c62186b587b4348750864e6175eb
karavainastya <134556049+karavainastya@users.noreply.github.com>
2023-06-15 01:56:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def main():
     NAME_INCOME_TYPE_Maternity_leave= st.selectbox('Тип дохода: В декретном отпуске',['0', '1']) 
     
     if NAME_INCOME_TYPE_Working ==1: 
-        NAME_INCOME_TYPE_State_servant & NAME_INCOME_TYPE_Commercial_associate & NAME_INCOME_TYPE_Pensioner & NAME_INCOME_TYPE_Unemployed & NAME_INCOME_TYPE_Student & NAME_INCOME_TYPE_Businessman & NAME_INCOME_TYPE_Businessman !=1
+        NAME_INCOME_TYPE_State_servant & NAME_INCOME_TYPE_Commercial_associate & NAME_INCOME_TYPE_Pensioner & NAME_INCOME_TYPE_Unemployed & NAME_INCOME_TYPE_Student & NAME_INCOME_TYPE_Businessman & NAME_INCOME_TYPE_Businessman ==0
         
     REGION_RATING_CLIENT= st.selectbox('Рейтинг региона проживания клиента: Примечание: 1 - Минск, 2 - областные центры, 3 - остальные населенные пункты.', ['1', '2', '3'])            
     REG_CITY_NOT_WORK_CITY = st.selectbox('Совпадает ли адрес клиента с адресом по прописке', ['0', '1']) 
