1b2071641d0bd04a6b8f00f979076d9adfe83f3c
karavainastya <134556049+karavainastya@users.noreply.github.com>
2023-06-15 08:50:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -115,20 +115,20 @@ def main():
     
     if int(NAME_INCOME_TYPE_Working) == 1: 
         (int(NAME_INCOME_TYPE_State_servant) & int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Pensioner) & int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Businessman) & int(NAME_INCOME_TYPE_Maternity_leave)) == 0
-    elif NAME_INCOME_TYPE_State_servant == 1:
-        (NAME_INCOME_TYPE_Working & NAME_INCOME_TYPE_Commercial_associate & NAME_INCOME_TYPE_Pensioner & NAME_INCOME_TYPE_Unemployed & NAME_INCOME_TYPE_Student & NAME_INCOME_TYPE_Businessman & NAME_INCOME_TYPE_Maternity_leave) == 0
-    elif NAME_INCOME_TYPE_Commercial_associate == 1:
-        (NAME_INCOME_TYPE_Working & NAME_INCOME_TYPE_State_servant & NAME_INCOME_TYPE_Pensioner & NAME_INCOME_TYPE_Unemployed & NAME_INCOME_TYPE_Student & NAME_INCOME_TYPE_Businessman & NAME_INCOME_TYPE_Maternity_leave) == 0
-    elif NAME_INCOME_TYPE_Pensioner == 1:
-        (NAME_INCOME_TYPE_Working & NAME_INCOME_TYPE_State_servant & NAME_INCOME_TYPE_Commercial_associate & NAME_INCOME_TYPE_Unemployed & NAME_INCOME_TYPE_Student & NAME_INCOME_TYPE_Businessman & NAME_INCOME_TYPE_Maternity_leave) == 0
-    elif NAME_INCOME_TYPE_Unemployed == 1:
-        (NAME_INCOME_TYPE_Working & NAME_INCOME_TYPE_State_servant & NAME_INCOME_TYPE_Commercial_associate & NAME_INCOME_TYPE_Pensioner & NAME_INCOME_TYPE_Student & NAME_INCOME_TYPE_Businessman & NAME_INCOME_TYPE_Maternity_leave) == 0
-    elif NAME_INCOME_TYPE_Student == 1:
-        (NAME_INCOME_TYPE_Working & NAME_INCOME_TYPE_State_servant & NAME_INCOME_TYPE_Commercial_associate & NAME_INCOME_TYPE_Pensioner & NAME_INCOME_TYPE_Unemployed & NAME_INCOME_TYPE_Businessman & NAME_INCOME_TYPE_Maternity_leave) == 0
-    elif NAME_INCOME_TYPE_Businessman == 1:
-        (NAME_INCOME_TYPE_Working & NAME_INCOME_TYPE_State_servant & NAME_INCOME_TYPE_Commercial_associate & NAME_INCOME_TYPE_Pensioner & NAME_INCOME_TYPE_Unemployed & NAME_INCOME_TYPE_Student & NAME_INCOME_TYPE_Maternity_leave) == 0
-    elif NAME_INCOME_TYPE_Maternity_leave == 1:
-        (NAME_INCOME_TYPE_Working & NAME_INCOME_TYPE_State_servant & NAME_INCOME_TYPE_Commercial_associate & NAME_INCOME_TYPE_Pensioner & NAME_INCOME_TYPE_Unemployed & NAME_INCOME_TYPE_Student & NAME_INCOME_TYPE_Businessman) == 0
+    elif int(NAME_INCOME_TYPE_State_servant) == 1:
+        (int(NAME_INCOME_TYPE_Working) & int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Pensioner) & int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Businessman) & int(NAME_INCOME_TYPE_Maternity_leave)) == 0
+    elif int(NAME_INCOME_TYPE_Commercial_associate) == 1:
+        (int(NAME_INCOME_TYPE_Working) & int(NAME_INCOME_TYPE_State_servant) & int(NAME_INCOME_TYPE_Pensioner) & int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Businessman) & int(NAME_INCOME_TYPE_Maternity_leave)) == 0
+    elif int(NAME_INCOME_TYPE_Pensioner) == 1:
+        (int(NAME_INCOME_TYPE_Working) & int(NAME_INCOME_TYPE_State_servant) & int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Businessman) & int(NAME_INCOME_TYPE_Maternity_leave)) == 0
+    elif int(NAME_INCOME_TYPE_Unemployed) == 1:
+        (int(NAME_INCOME_TYPE_Working) & int(NAME_INCOME_TYPE_State_servant) & int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Pensioner) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Businessman) & int(NAME_INCOME_TYPE_Maternity_leave)) == 0
+    elif int(NAME_INCOME_TYPE_Student) == 1:
+        (int(NAME_INCOME_TYPE_Working) & int(NAME_INCOME_TYPE_State_servant) & int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Pensioner) & int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Businessman) & int(NAME_INCOME_TYPE_Maternity_leave)) == 0
+    elif int(NAME_INCOME_TYPE_Businessman) == 1:
+        (int(NAME_INCOME_TYPE_Working) & int(NAME_INCOME_TYPE_State_servant) & int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Pensioner) & int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Maternity_leave)) == 0
+    elif int(NAME_INCOME_TYPE_Maternity_leave) == 1:
+        (int(NAME_INCOME_TYPE_Working) & int(NAME_INCOME_TYPE_State_servant) & int(NAME_INCOME_TYPE_Commercial_associate) & int(NAME_INCOME_TYPE_Pensioner) & int(NAME_INCOME_TYPE_Unemployed) & int(NAME_INCOME_TYPE_Student) & int(NAME_INCOME_TYPE_Businessman))== 0
     else:
         st.error('Некорректный ввод данных по типу дохода.')
     
