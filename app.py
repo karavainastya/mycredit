5f246eb3c00f56b6470393ac6474638de284950a
karavainastya <134556049+karavainastya@users.noreply.github.com>
2023-06-18 22:21:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def set_png_as_page_bg(png_file):
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
-set_png_as_page_bg('default1.jpg')
+set_png_as_page_bg('fon.jpg')
 
 
 #classifier_name=['LogisticRegression', 'LightGBM']
